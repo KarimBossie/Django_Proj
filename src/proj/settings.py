4b21e6440c6701e98fbb1f63cc8ca32978074be6
Karim S <karimsub@outlook.com>
2022-07-06 18:29:01 +0300

    commit

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^y9j&j5md!29z9leqbz9*pz8)d40b*u4_14a$wtet7i%0j=_8u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://lilbossie69.pythonanywhere.com/"]
 
 
 # Application definition
