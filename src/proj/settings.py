b04341188b30e22e599fd5a7cd980942128bc5bb
Karim S <karimsub@outlook.com>
2022-07-06 19:41:26 +0300

    ocmmit

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -124,3 +124,4 @@ MEDIA_ROOT = '/home/LilBossie69/Django_Proj/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
