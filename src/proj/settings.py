5395a4335bfa7c2e1c064d05674eb0a2ecdfd4d4
Karim S <karimsub@outlook.com>
2022-07-06 18:39:36 +0300

    commit

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'home/LilBossie69/Django_Proj/static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'home/LilBossie69/Django_Proj/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
