e77b5d332cd134d9782aa19d49b9549f96ea7c1b
Karim S <karimsub@outlook.com>
2022-08-22 18:45:06 +0300

    commit

--- src/DjanGoBookStoRe/models.py
+++
@@ -1,5 +0,0 @@
-from tabnanny import verbose
-from django.db import models
-
-# Create your models here.
-
--- src/DjanGoBookStoRe/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from . import models
-
-# Create your views here.
